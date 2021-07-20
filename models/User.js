cb4eb867a24d36901f78111864b9a2edf9df9403
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-20 02:06:32 -0700

    initial setup for app for dev branch

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        //define Columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          username: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [4]
+            }
+          }
+    },
+    {
+        sequelize
+    }
+);
