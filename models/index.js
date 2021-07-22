90c556af7236c26492eac62debd806bcf587aff3
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-22 13:21:15 -0700

    added associations between models

--- models/index.js
+++ models/index.js
@@ -7,31 +7,51 @@ const FeatureTag = require('./FeatureTag');
 const Size = require('./Size');
 const Appointment = require('./Appointment');
 
-//User can haveMany Grill objects
-// User.hasMany(Grill);
-Grill.belongsToMany(User, {
-   through: Grill,
+//User can have multiple Griils
+Grill.belongsTo(User, {
    foreignKey:'owner_id'
 });
 
-User.belongsTo(Grill, {
-   through: Grill,
+//Grills can belong to one User
+User.hasMany(Grill, {
    foreignKey: 'owner_id'
 });
 
+//Griils has one Category
 Grill.belongsTo(Category, {
    foreignKey: 'category_id'
 });
 
+//Category can be on more than one Grill
 Category.hasMany(Grill, {
    foreignKey: 'category_id'
 })
 
+//Grills cam have one Sizes
+Grill.belongsTo(Size, {
+   foreignKey: 'size_id'
+});
+
+Size.hasOne(Grill, {
+   foreignKey: 'size_id'
+});
+
+//Grill can have one brand
+Grill.belongsTo(Brand, {
+   foreignKey: 'brand_id'
+});
+
+Brand.hasOne(Grill, {
+   foreignKey: 'brand_id'
+});
+
+//Grill Can have many feature tags
 Grill.belongsToMany(FeatureTag, {
    through: Feature,
    foreignKey: 'grill_id'
 });
 
+//FeatureTag Can have manny grills
 FeatureTag.belongsToMany(Grill, {
    through: Feature,
    foreignKey: 'tag_id'
