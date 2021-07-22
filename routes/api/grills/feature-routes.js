9b75df6bf381c3d1eb845dd7bfb31715ef517e9d
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-22 02:32:49 -0700

    layout for all routes for api

---
+++ routes/api/grills/feature-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {  }  = require('../../../models');
+
+router.get('/', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
