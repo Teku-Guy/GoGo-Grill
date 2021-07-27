cfce59821f646d0e534ef5c35b469bd97d6fdb16
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-26 19:42:41 -0700

    update brand

--- routes/api/grills/brand-routes.js
+++ routes/api/grills/brand-routes.js
@@ -1,8 +1,38 @@
 const router = require('express').Router();
-const { Brand }  = require('../../../models');
+const { Grill, Category, Size, Brand, User, FeatureTag, Feature  }  = require('../../../models');
+
+//The `api/brands/` endpoint
 
 //get all brands and related grill groups
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    Brand.findAll({
+        include: [
+            {
+                model: Grill,
+                attributes: ['id', 'owner_id', 'createdAt',],
+                include: [
+                    {
+                        model: Brand,
+                        attributes: [['brand_name', 'name']]
+                    },
+                    {
+                        model: Category,
+                        attributes: [['category_name', 'type']]
+                    },
+                    {
+                        model: Size,
+                        attributes: ['dimensions']
+                    },
+                    {
+                        model: FeatureTag,
+                        attributes: [['feature_name', 'feature']],
+                        through: {attributes: []}
+                    }
+                ]
+            }
+        ]
+    })
+});
 
 //get brand by id
 router.get('/:id', (req, res) => {});
