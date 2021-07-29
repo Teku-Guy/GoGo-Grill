a092ab5f2356b5a418aacf78dd58bfae33d44e97
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-29 04:04:32 -0700

    signup/login route done visually

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+// $(function () {
+//     $("#bday").datepicker({ 
+//           autoclose: true, 
+//           todayHighlight: true
+//     }).datepicker('update', new Date());;
+//   });
+// $(document).ready(function() {  
+//       $('.datepicker').datepicker();  
+// });
