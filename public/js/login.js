cc56d7349793d13d792ebd208347880f4ea131a4
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-29 04:41:23 -0700

    update visually how login routes looks like

--- public/js/login.js
+++
@@ -1,9 +0,0 @@
-// $(function () {
-//     $("#bday").datepicker({ 
-//           autoclose: true, 
-//           todayHighlight: true
-//     }).datepicker('update', new Date());;
-//   });
-// $(document).ready(function() {  
-//       $('.datepicker').datepicker();  
-// });
